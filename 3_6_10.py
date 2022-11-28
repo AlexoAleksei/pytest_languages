2971409164f1f7c8de8009adb83ae0ab3c41897e
AlexoAleksei <xanlexan100@gmail.com>
2022-11-28 20:38:12 +0300

    Updated test script

--- 3_6_10.py
+++ 3_6_10.py
@@ -5,6 +5,6 @@ from selenium.webdriver.common.by import By
 
 def test_languages(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(30)
     button_add_to_basket = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button_add_to_basket, "Button adding to basket does not exist"
-    time.sleep(5)

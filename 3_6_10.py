425cdd53e7bc97d655f00873bee2e5b9e447f62a
AlexoAleksei <xanlexan100@gmail.com>
2022-11-28 20:35:36 +0300

    Updated test script

--- 3_6_10.py
+++ 3_6_10.py
@@ -5,8 +5,6 @@ from selenium.webdriver.common.by import By
 
 def test_languages(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    button_add_to_basket = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    if browser.find_element(By.CSS_SELECTOR, "[name='language'] > [value='fr']").get_attribute("selected"): #Выставлен французский язык
-        print("French language selected")
-        assert button_add_to_basket.text == "Ajouter au panier", "Button adding to basket has wrong text in french"
-    time.sleep(30)
+    button_add_to_basket = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert button_add_to_basket, "Button adding to basket does not exist"
+    time.sleep(5)
